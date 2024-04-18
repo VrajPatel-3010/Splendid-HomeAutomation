8b45eb185c65695f8374b75047ef892462fff849
Vraj Patel <patel.vraj7255@gmail.com>
2024-04-18 14:33:15 -0700

    Commit V1

---
+++ Main Files/ingenious/js/email.js
@@ -0,0 +1,34 @@
+(function () {
+	emailjs.init('nqzxQS6LeuJNLlbzs');
+})();
+
+if ($('#email-form').length) {
+	$('#submit').click(function () {
+		var username = $('#email-form .username').val();
+		var email = $('#email-form .email').val();
+		var contact_message = $('#email-form .contact_message').val();
+
+		if (username == '' || email == '') {
+			$('#email-form .response').html('<div class="failed">Please fill the required fields.</div>');
+			return false;
+		}
+		$('#email-form .response').html('<div class="text-info"><img src="images/icons/preloader.gif"> Loading...</div>');
+		//Default
+		const serviceID = 'service_ivhx959';
+		const templateID = 'template_je2vqdu';
+
+		var contactParams = {
+			username: username,
+			mail: email,
+			message: contact_message,
+		};
+		emailjs.send(serviceID, templateID, contactParams)
+			.then(function () {
+				console.log('Mail Sent !');
+				$('#email-form .response').html('<div class="success">Email has been sent successfully.</div>');
+			}, function (error) {
+				console.log('FAILED...', error);
+				$('#email-form .response').html('<div class="error">Error: Email has not been sent.');
+			});
+	});
+}
---
+++ Main Files/ingenious/sendemail.php
@@ -0,0 +1,43 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+
+if (isset($_POST["username"])) {
+    $mail = new PHPMailer(true); // Passing `true` enables exceptions
+
+    try {
+        // Server settings
+        // $mail->SMTPDebug = 2; // Enable verbose debug output
+        $mail->isSMTP(); // Set mailer to use SMTP
+        $mail->Host = 'relay-hosting.secureserver.net'; // Specify GoDaddy's SMTP server
+        $mail->SMTPAuth = false; // Disable SMTP authentication
+        $mail->Port = 25; // TCP port to connect to
+
+        // Recipients
+        $mail->setFrom('info@splendidit.com', 'Mailer');
+        $mail->addAddress('info@splendidit.com', 'Recipient Name'); // Add a recipient
+
+        // Content
+        $mail->isHTML(true); // Set email format to HTML
+        $userName = isset($_POST['username']) ? preg_replace("/[^\s\S\.\-\_\@a-zA-Z0-9]/", "", $_POST['username']) : "";
+        $senderEmail = isset($_POST['email']) ? preg_replace("/[^\.\-\_\@a-zA-Z0-9]/", "", $_POST['email']) : "";
+        $userMessage = isset($_POST['contact_message']) ? preg_replace("/(From:|To:|BCC:|CC:|Subject:|Content-Type:)/", "", $_POST['contact_message']) : "";
+        $messageBody = "Name: " . $userName . "<br>Email: " . $senderEmail . "<br>Message: " . $userMessage;
+
+        $mail->Subject = 'Contact Us';
+        $mail->Body    = $messageBody;
+        $mail->AltBody = strip_tags($messageBody);
+
+        $mail->send();
+        echo '<div class="success">Email has been sent successfully.</div>';
+    } catch (Exception $e) {
+        echo '<div class="error">Error: Email has not been sent. ' . $e->getMessage() . '</div>';
+    }
+} else {
+    echo '<div class="failed">Failed sending your email.</div>';
+}
+?>
